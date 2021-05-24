ec7c31322217b24aa84c8df668f1353f16f503d7
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-05-24 21:08:14 +0300

    Adding list of items on landing page and what reflect the outpu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubComponent } from './github/github.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [];
 
