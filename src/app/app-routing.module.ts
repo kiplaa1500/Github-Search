21aa381cdace57e9e5b7c00816e084a978756cf9
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-05-24 21:11:29 +0300

    Adding routes to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { GithubComponent } from './github/github.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [];
+{ path: 'git', component: GithubComponent },
+{ path: 'about', component: AboutComponent },
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
