4d748dc8e9df2469026f9861b916a2b51ab2cd48
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-05-24 20:50:48 +0300

    Getting return from its inputs

--- src/app/service/github.service.ts
+++ src/app/service/github.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  private username: string;
+  constructor(private http: HttpClient) {
+    this.username = 'kiplaa1500';
+  }
+  getProfileInformation() {
+    return this.http.get(`https://api.github.com/users/${this.username}?}`)
+  }
+  getProfileRepos() {
+    return this.http.get(`https://api.github.com/users/${this.username}/repos?}`)
+  }
+  updateUserProfile(username: string) {
+    this.username = username;
+  }
 }
